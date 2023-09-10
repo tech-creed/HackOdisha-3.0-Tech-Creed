a97f4b2d49bedc4731ef1c4edd355b52ff4ea96d
MUKILAN S <mukilan069@gmail.com>
2023-09-10 13:38:24 +0530

    pytesseract Iplemented Backend -- Complected

--- ml-backend/views/views.py
+++ ml-backend/views/views.py
@@ -1,6 +1,11 @@
 from flask import jsonify, request
+from PIL import Image
+import pytesseract
+
 from app import *
 
 @app.route('/', methods = ['GET', 'POST'])
-def home():
-    return jsonify({'data': 'hello'})
+def home(imagePath):
+    data = pytesseract.image_to_string(Image.open('test.png'))
+
+    return jsonify({'data': data})
